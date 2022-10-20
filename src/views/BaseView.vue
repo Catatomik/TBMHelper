941f6fba6757148feabcc014f8ac203167c4b90a
Catatomik <cata@catadev.org>
2022-10-21 01:28:42 +0200

    🚧 Correctly remove stops

--- src/views/BaseView.vue
+++ src/views/BaseView.vue
@@ -32,8 +32,8 @@ async function queryUpdated(to = route) {
     to.query["excludedStopPoints"] === undefined
       ? []
       : to.query["excludedStopPoints"] instanceof Array
-      ? to.query["excludedStopPoints"]
-      : [to.query["excludedStopPoints"]]
+        ? to.query["excludedStopPoints"]
+        : [to.query["excludedStopPoints"]]
   ) as `${StopArea["id"]}-${StopPoint["id"]}`[];
 
   for (const k of Object.keys(to.query)
@@ -93,16 +93,24 @@ function removeStop(stop: FullyDescribedStop) {
   selectedStops.value = selectedStops.value.filter((s) => s.id != stop.id);
 
   let queryNeedUpdate = false;
+
+  if (excludedStopPoints.value.find((s) => s.startsWith(stop.id))) {
+    queryNeedUpdate = true;
+    excludedStopPoints.value = excludedStopPoints.value.filter((s) => !s.startsWith(stop.id));
+    query["excludedStopPoints"] = excludedStopPoints.value;
+  }
+
   Object.keys(query).forEach((k) => {
     if (query[k] === stop.name) {
       queryNeedUpdate = true;
       delete query[k];
     }
-    if (queryNeedUpdate) {
-      queryInternallyUpdated = true;
-      router.push({ query });
-    }
   });
+
+  if (queryNeedUpdate) {
+    queryInternallyUpdated = true;
+    router.push({ query });
+  }
 }
 
 function removeStopPoint(stopArea: StopArea, stopPoint: StopPoint) {
@@ -120,15 +128,10 @@ function removeStopPoint(stopArea: StopArea, stopPoint: StopPoint) {
   <main>
     <div class="flex flex-col">
       <div class="flex justify-center">
-        <input
-          class="my-3 p-1 w-2/3 border-2 border-slate-500 rounded-md shadow-md"
-          type="text"
-          placeholder="Chercher un arrêt..."
-          list="selectedStops"
-          :value="stopInput"
+        <input class="my-3 p-1 w-2/3 border-2 border-slate-500 rounded-md shadow-md" type="text"
+          placeholder="Chercher un arrêt..." list="selectedStops" :value="stopInput"
           @input="(stopInput = ($event.target as HTMLInputElement).value), refreshStops()"
-          @keyup.enter="addCurrentStop()"
-        />
+          @keyup.enter="addCurrentStop()" />
 
         <datalist id="selectedStops">
           <option v-for="stop in stops" :key="stop.id" :value="stop.name" />
@@ -136,31 +139,26 @@ function removeStopPoint(stopArea: StopArea, stopPoint: StopPoint) {
       </div>
       <h3
         v-if="!selectedStops.reduce((acc, val) => [...acc, ...val.details.stopPoints.filter(sp => !excludedStopPoints.includes(`${val.id}-${sp.id}`))], [] as StopPoint[]).length"
-        class="mt-5 text-center font-bold text-lg"
-      >
+        class="mt-5 text-center font-bold text-lg">
         Aucun arrêt sélectionné
       </h3>
       <div v-else class="my-5 mx-2 grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         <StopPointComp
           v-for="stopPoint in selectedStops.reduce((acc, val) => [...acc, ...val.details.stopPoints.filter(sp => !excludedStopPoints.includes(`${val.id}-${sp.id}`))], [] as StopPoint[])"
-          :key="stopPoint.id"
-          :stop-point="stopPoint"
-          @soft-delete="
+          :key="stopPoint.id" :stop-point="stopPoint" @soft-delete="
             removeStopPoint(
               selectedStops.find((s) =>
                 s.details.stopPoints.find((sp) => sp.id === stopPoint.id),
               ) as FullyDescribedStop,
               stopPoint,
             )
-          "
-          @hard-delete="
+          " @hard-delete="
             removeStop(
               selectedStops.find((s) =>
                 s.details.stopPoints.find((sp) => sp.id === stopPoint.id),
               ) as FullyDescribedStop,
             )
-          "
-        />
+          " />
       </div>
     </div>
   </main>

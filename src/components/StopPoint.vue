d6393304f59cbeb190da5e382e775bb0d56cead8
Catatomik <cata@catadev.org>
2022-11-17 10:23:03 +0100

    ♿️ Doc buttons

--- src/components/StopPoint.vue
+++ src/components/StopPoint.vue
@@ -97,10 +97,11 @@ function refreshRouteRealtime(route: OperatingRoute, intId?: number) {
 
 <template>
   <div class="rounded-lg p-3 shadow-xl">
-    <div class="flex justify-center items-center">
-      <h3 class="text-center font-bold text-lg">📍 {{ stopPoint.name }}</h3>
-      <button class="ml-2 border-4 rounded-lg border-orange-400 text-center select-none" @click="emit('softDelete')">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="w-4 h-4 fill-orange-400">
+    <div class="flex items-center">
+      <h3 class="text-center font-bold text-lg mx-auto">📍 {{ stopPoint.name }}</h3>
+      <button class="ml-2 border-4 rounded-lg border-orange-400 text-center select-none justify-self-end"
+        @click="emit('softDelete')">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="w-4 h-4 fill-orange-400 justify-self-end">
           <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
           <path
             d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z" />

13ec28480c56e085f606ea519e135e2075d41c4b
Catatomik <cata@catadev.org>
2022-11-19 11:51:27 +0100

    🎨 Formatting

--- src/components/CloseButton.vue
+++ src/components/CloseButton.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-
 interface Props {
   borderColor?: string;
   fillColor?: string;
 }
 
 withDefaults(defineProps<Props>(), { borderColor: "border-red-500", fillColor: "fill-red-500" });
-
 </script>
 
 <template>
@@ -14,7 +12,8 @@ withDefaults(defineProps<Props>(), { borderColor: "border-red-500", fillColor: "
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="w-4 h-4" :class="[fillColor]">
       <!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
       <path
-        d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z" />
+        d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"
+      />
     </svg>
   </button>
 </template>

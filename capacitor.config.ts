785b491e99630cf2ffa0cd892b0a1b85d19df81f
Catatomik <cata@catadev.org>
2022-11-27 13:13:11 +0100

    ➕ Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.catadev.tbmhelper",
+  appName: "TBM Helper",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;

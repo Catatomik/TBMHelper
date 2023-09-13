224615641ab48faa22b4a7aeb6dbc2f194787b54
Catatomik <cata@catadev.org>
2023-09-13 21:29:13 +0200

    ✨ Destination selector

--- src/components/RouteHeader.vue
+++ src/components/RouteHeader.vue
@@ -1,34 +1,75 @@
 <script setup lang="ts">
 import type { OperatingRoute } from "@/store/TBM";
+import { ref, type Ref } from "vue";
 
 export interface RouteHeader {
   route: OperatingRoute;
+  destSelect?: boolean;
 }
-const props = defineProps<RouteHeader>();
+const props = withDefaults(defineProps<RouteHeader>(), {
+  destSelect: false,
+});
+
+export type Checked = Record<
+  OperatingRoute["stopPointDetails"]["schedules"]["destinations"][number],
+  boolean
+>;
+
+const checked: Ref<Checked> = ref(
+  props.route.stopPointDetails.schedules.destinations.reduce((acc, v) => ({ ...acc, [v]: true }), {}),
+);
+
+const emit = defineEmits<{
+  (e: "update:checked", checked: Checked): void;
+}>();
+
+emit("update:checked", checked.value);
 </script>
 
 <template>
-  <img
-    v-if="'id' in props.route.lineDetails"
-    width="25"
-    class="inline align-middle"
-    :src="props.route.lineDetails.picto"
-  />
-  <p class="mx-1 inline align-middle">
-    {{
-      props.route.stopPointDetails.route.line.type === "Bus" ||
-      props.route.stopPointDetails.route.line.type === "Autocar" ||
-      props.route.stopPointDetails.route.line.type === "Bus Scolaire"
-        ? "🚌"
-        : props.route.stopPointDetails.route.line.type === "Tramway"
-        ? "🚋"
-        : props.route.stopPointDetails.route.line.type === "Train régional / TER"
-        ? "🚆"
-        : ""
-    }}
-  </p>
-  <h4 class="font-bold text-base py-1 inline align-middle">
-    {{ props.route.line.name }}
-  </h4>
-  <p class="inline align-middle ml-1">➜ {{ props.route.name }}</p>
+  <div>
+    <img
+      v-if="'id' in route.lineDetails"
+      width="25"
+      class="inline align-middle"
+      :src="route.lineDetails.picto"
+    />
+    <p class="mx-1 inline align-middle">
+      {{
+        route.stopPointDetails.route.line.type === "Bus" ||
+        route.stopPointDetails.route.line.type === "Autocar" ||
+        route.stopPointDetails.route.line.type === "Bus Scolaire"
+          ? "🚌"
+          : route.stopPointDetails.route.line.type === "Tramway"
+          ? "🚋"
+          : route.stopPointDetails.route.line.type === "Train régional / TER"
+          ? "🚆"
+          : ""
+      }}
+    </p>
+    <h4 class="font-bold text-base py-1 inline align-middle">
+      {{ route.line.name }}
+    </h4>
+  </div>
+  <div v-if="destSelect" class="mt-1">
+    <div v-for="destination in route.stopPointDetails.schedules.destinations" :key="destination" class="mt-1">
+      <span
+        class="dest rounded px-1"
+        :class="[
+          route.stopPointDetails.schedules.destinations.length > 1
+            ? `dest-${route.stopPointDetails.schedules.destinations.indexOf(destination).toLocaleString()}`
+            : 'border-transparent',
+        ]"
+      >
+        <input
+          v-if="route.stopPointDetails.schedules.destinations.length > 1"
+          v-model="checked[destination]"
+          type="checkbox"
+          @input="emit('update:checked', checked)"
+        />
+        <p v-else class="inline">➜</p>
+        {{ destination }}
+      </span>
+    </div>
+  </div>
 </template>
